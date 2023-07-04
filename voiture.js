46f5970d5b2b27e7e06bbc2ced6780bbb39b06e2
Jose <josemiguelvalecosta@gmail.com>
2023-07-04 14:26:23 +0200

    etoile center

--- voiture.js
+++ voiture.js
@@ -241,7 +241,9 @@ document.querySelector("#ma-question-2").addEventListener("submit", function (ev
                 charles[index].classList.remove("charles-active");
                 etoileentiere3.classList.add("etoile-entiere-3-active");
             }, 2000, i);
-
+            setTimeout((index) => {
+                fin.classList.add("fin-active");
+            }, 5000, i);
 
         };
         q2.innerText = "Bonne réponse !!!";
@@ -302,6 +304,9 @@ document.querySelector("#ma-question-3").addEventListener("submit", function (ev
                 charles[index].classList.remove("charles-active");
                 etoileentiere3.classList.add("etoile-entiere-3-active");
             }, 2000, i);
+            setTimeout((index) => {
+                fin.classList.add("fin-active");
+            }, 5000, i);
 
 
         };
